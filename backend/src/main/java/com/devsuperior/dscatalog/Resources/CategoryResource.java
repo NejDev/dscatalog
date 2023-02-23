d210695643c813081f6ed6e286e7a89e790ca00e
paulo <nejeldev@gmail.com>
2023-02-23 14:49:58 -0300

    categoryResource

--- backend/src/main/java/com/devsuperior/dscatalog/Resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/Resources/CategoryResource.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,8 +17,8 @@ public class CategoryResource {
    @GetMapping
     public ResponseEntity<List<Category>> findAll(){
         List<Category> list = new ArrayList<>();
-        list.add(new Category(long, "Books"));
-        list.add(new Category(long, "Electonics"));
+        list.add(new Category( 1L, "Books"));
+        list.add(new Category( 2L, "Electonics"));
         return ResponseEntity.ok().body(list);
     }
 }
