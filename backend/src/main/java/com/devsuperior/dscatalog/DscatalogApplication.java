4c2b96a7b86d6fc4572c815c97af2af36b7aece8
paulo <nejeldev@gmail.com>
2023-03-08 20:03:04 -0300

    LocalHost FUNCIONANDO PERFEITAMENTE

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -2,7 +2,7 @@ package com.devsuperior.dscatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class  DscatalogApplication {
