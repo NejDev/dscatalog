ee6237d975b2f2e79a8c7ede180198531acb51d2
paulo <nejeldev@gmail.com>
2023-03-24 15:48:25 -0300

    DTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/DTO/CategoryDTO.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscatalog.DTO;
+
+
+import com.devsuperior.dscatalog.entities.Category;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class CategoryDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    public CategoryDTO() {
+    }
+
+    public CategoryDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity){
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+}
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,25 +1,32 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.DTO.CategoryDTO;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.devsuperior.dscatalog.entities.Category;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
     @Autowired
-private CategoryRepository repository;
+    private CategoryRepository repository;
 
-   @Transactional(readOnly = true)
-public List<Category> findAll(){
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
 
-       return repository.findAll();
-}
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+        }
+    }
 
 
 
 
-}
+
+
